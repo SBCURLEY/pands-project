208bea48492c094d4e1727bae65cd4e47b697b00
Sharon Curley <sbrogancurley@gmail.com>
2024-04-02 05:33:31 +0100

    Research Project

--- analysis.py
+++ analysis.py
@@ -2,10 +2,4 @@
 #
 # Author: Sharon Curley
 
-Research
-Python – Basics of Pandas using Iris Dataset      https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/
-Iris Dataset Classification with Python: A Tutorial     https://www.pycodemates.com/2022/05/iris-dataset-classification-with-python.html?utm_content=cmp-true
 
-
-
-iris picture
